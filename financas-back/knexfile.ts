74dba3d63d26533bc46f3eae2cd6103a66023a3d
daniellemes2017 <daniel_lemes2017@outlook.com>
2024-11-06 11:21:13 -0300

    atualização do codigo

--- financas-back/knexfile.ts
+++ financas-back/knexfile.ts
@@ -4,12 +4,8 @@ import 'dotenv/config';
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || '@Dani9140',
-      database: process.env.DB_NAME || 'gestao_financas',
-    },
+    connection: process.env.DATABASE_URL,
+  
     migrations: {
       directory: './src/database/migrations',
     },
